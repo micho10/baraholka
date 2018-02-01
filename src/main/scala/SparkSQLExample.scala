a91a3a3c7ca6b96d91ce0f6978146d9ace55f34c
carlos.sainz <carlos.sainz@globalsign.com>
2018-02-01 19:31:42 +0900

    Added basic example

--- src/main/scala/SparkSQLExample.scala
+++ src/main/scala/SparkSQLExample.scala
@@ -3,14 +3,13 @@ import org.apache.spark.sql.SparkSession
 object SparkSQLExample {
 
   def main(args: Array[String]) {
-    // $example on:init_session$
     val spark = SparkSession
       .builder()
       .appName("Spark SQL basic example")
       .config("spark.some.config.option", "some-value")
       .getOrCreate()
 
-//    runBasicDataFrameExample(spark)
+    runBasicDataFrameExample(spark)
 //    runDatasetCreationExample(spark)
 //    runInferSchemaExample(spark)
 //    runProgrammaticSchemaExample(spark)
@@ -19,107 +18,102 @@ object SparkSQLExample {
   }
 
 
-//  private def runBasicDataFrameExample(spark: SparkSession): Unit = {
-//    val df = spark.read.json("examples/src/main/resources/people.json")
-//
-//    // Displays the content of the DataFrame to stdout
-//    df.show()
-//    // +----+-------+
-//    // | age|   name|
-//    // +----+-------+
-//    // |null|Michael|
-//    // |  30|   Andy|
-//    // |  19| Justin|
-//    // +----+-------+
-//
-//    // For implicit conversions like converting RDDs to DataFrames
-//    // This import is needed to use the $-notation
-//    import spark.implicits._
-//    // Print the schema in a tree format
-//    df.printSchema()
-//    // root
-//    // |-- age: long (nullable = true)
-//    // |-- name: string (nullable = true)
-//
-//    // Select only the "name" column
-//    df.select("name").show()
-//    // +-------+
-//    // |   name|
-//    // +-------+
-//    // |Michael|
-//    // |   Andy|
-//    // | Justin|
-//    // +-------+
-//
-//    // Select everybody, but increment the age by 1
-//    df.select($"name", $"age" + 1).show()
-//    // +-------+---------+
-//    // |   name|(age + 1)|
-//    // +-------+---------+
-//    // |Michael|     null|
-//    // |   Andy|       31|
-//    // | Justin|       20|
-//    // +-------+---------+
-//
-//    // Select people older than 21
-//    df.filter($"age" > 21).show()
-//    // +---+----+
-//    // |age|name|
-//    // +---+----+
-//    // | 30|Andy|
-//    // +---+----+
-//
-//    // Count people by age
-//    df.groupBy("age").count().show()
-//    // +----+-----+
-//    // | age|count|
-//    // +----+-----+
-//    // |  19|    1|
-//    // |null|    1|
-//    // |  30|    1|
-//    // +----+-----+
-//    // $example off:untyped_ops$
-//
-//    // $example on:run_sql$
-//    // Register the DataFrame as a SQL temporary view
-//    df.createOrReplaceTempView("people")
-//
-//    val sqlDF = spark.sql("SELECT * FROM people")
-//    sqlDF.show()
-//    // +----+-------+
-//    // | age|   name|
-//    // +----+-------+
-//    // |null|Michael|
-//    // |  30|   Andy|
-//    // |  19| Justin|
-//    // +----+-------+
-//    // $example off:run_sql$
-//
-//    // $example on:global_temp_view$
-//    // Register the DataFrame as a global temporary view
-//    df.createGlobalTempView("people")
-//
-//    // Global temporary view is tied to a system preserved database `global_temp`
-//    spark.sql("SELECT * FROM global_temp.people").show()
-//    // +----+-------+
-//    // | age|   name|
-//    // +----+-------+
-//    // |null|Michael|
-//    // |  30|   Andy|
-//    // |  19| Justin|
-//    // +----+-------+
-//
-//    // Global temporary view is cross-session
-//    spark.newSession().sql("SELECT * FROM global_temp.people").show()
-//    // +----+-------+
-//    // | age|   name|
-//    // +----+-------+
-//    // |null|Michael|
-//    // |  30|   Andy|
-//    // |  19| Justin|
-//    // +----+-------+
-//    // $example off:global_temp_view$
-//  }
+  private def runBasicDataFrameExample(spark: SparkSession): Unit = {
+    val df = spark.read.json("examples/src/main/resources/people.json")
+
+    // Displays the content of the DataFrame to stdout
+    df.show()
+    // +----+-------+
+    // | age|   name|
+    // +----+-------+
+    // |null|Michael|
+    // |  30|   Andy|
+    // |  19| Justin|
+    // +----+-------+
+
+    // For implicit conversions like converting RDDs to DataFrames
+    // This import is needed to use the $-notation
+    import spark.implicits._
+    // Print the schema in a tree format
+    df.printSchema()
+    // root
+    // |-- age: long (nullable = true)
+    // |-- name: string (nullable = true)
+
+    // Select only the "name" column
+    df.select("name").show()
+    // +-------+
+    // |   name|
+    // +-------+
+    // |Michael|
+    // |   Andy|
+    // | Justin|
+    // +-------+
+
+    // Select everybody, but increment the age by 1
+    df.select($"name", $"age" + 1).show()
+    // +-------+---------+
+    // |   name|(age + 1)|
+    // +-------+---------+
+    // |Michael|     null|
+    // |   Andy|       31|
+    // | Justin|       20|
+    // +-------+---------+
+
+    // Select people older than 21
+    df.filter($"age" > 21).show()
+    // +---+----+
+    // |age|name|
+    // +---+----+
+    // | 30|Andy|
+    // +---+----+
+
+    // Count people by age
+    df.groupBy("age").count().show()
+    // +----+-----+
+    // | age|count|
+    // +----+-----+
+    // |  19|    1|
+    // |null|    1|
+    // |  30|    1|
+    // +----+-----+
+
+    // Register the DataFrame as a SQL temporary view
+    df.createOrReplaceTempView("people")
+
+    val sqlDF = spark.sql("SELECT * FROM people")
+    sqlDF.show()
+    // +----+-------+
+    // | age|   name|
+    // +----+-------+
+    // |null|Michael|
+    // |  30|   Andy|
+    // |  19| Justin|
+    // +----+-------+
+
+    // Register the DataFrame as a global temporary view
+    df.createGlobalTempView("people")
+
+    // Global temporary view is tied to a system preserved database `global_temp`
+    spark.sql("SELECT * FROM global_temp.people").show()
+    // +----+-------+
+    // | age|   name|
+    // +----+-------+
+    // |null|Michael|
+    // |  30|   Andy|
+    // |  19| Justin|
+    // +----+-------+
+
+    // Global temporary view is cross-session
+    spark.newSession().sql("SELECT * FROM global_temp.people").show()
+    // +----+-------+
+    // | age|   name|
+    // +----+-------+
+    // |null|Michael|
+    // |  30|   Andy|
+    // |  19| Justin|
+    // +----+-------+
+  }
 
 
 }
